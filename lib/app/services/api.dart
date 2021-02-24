00dea26d21c82c0c4e52b1599cd18bb908f9ce9c
Ujjwal Sharma <ujjwal3sharma@gmail.com>
2021-02-24 22:38:35 +0530

    gdfg

---
+++ lib/app/services/api.dart
@@ -0,0 +1,18 @@
+import 'package:covid19_app/app/services/api_keys.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter/foundation.dart';
+
+class API {
+  API({@required this.apiKey});
+  final String apiKey;
+
+  factory API.sandbox() => API(apiKey: APIKeys.ncovSandboxKey);
+
+  static final String host = 'ncov2019-admin.firebaseapp.com';
+
+  Uri tokenUri() => Uri(
+        scheme: 'https',
+        host: host,
+        path: 'token',
+      );
+}
